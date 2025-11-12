82aa01ac71b6a2feb7abbd4acddaf67b73c63d34
AlexKuryanov <lexxus07@yandex.ru>
2025-11-12 11:09:22 +0100

    refactor: set  incapsulation to js-files

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,17 @@
-const modalBtn = document.querySelector('.modal__button');
-const modal = document.querySelector('.modal');
+const modal = () => {
+  const modalBtn = document.querySelector('.modal__button');
+  const modal = document.querySelector('.modal');
 
-modalBtn.addEventListener('click', () => {
-  modal.style.display = 'flex';
-});
+  modalBtn.addEventListener('click', () => {
+    modal.style.display = 'flex';
+  });
 
-modal.addEventListener('click', (e) => {
-  const modalContent = e.target.closest('.modal__inner');
-  if (!modalContent) {
-    modal.style.display = '';
-  }
-});
+  modal.addEventListener('click', (e) => {
+    const modalContent = e.target.closest('.modal__inner');
+    if (!modalContent) {
+      modal.style.display = '';
+    }
+  });
+};
+
+modal();
--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,18 +1,22 @@
-const navBar = document.querySelector('.header__nav');
-const links = navBar.querySelectorAll('a');
+const smoothScroll = () => {
+  const navBar = document.querySelector('.header__nav');
+  const links = navBar.querySelectorAll('a');
 
-links.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
+  links.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
 
-    const section = document.querySelector(link.getAttribute('href'));
+      const section = document.querySelector(link.getAttribute('href'));
 
-    if (section) {
-      seamless.scrollIntoView(section, {
-        behavior: 'smooth',
-        block: 'center',
-        inline: 'center',
-      });
-    }
+      if (section) {
+        seamless.scrollIntoView(section, {
+          behavior: 'smooth',
+          block: 'center',
+          inline: 'center',
+        });
+      }
+    });
   });
-});
+};
+
+smoothScroll();
